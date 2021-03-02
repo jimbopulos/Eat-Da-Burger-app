0c55828f2e225bf87877e512de4a8b6de1825502
James Galantino <james_galantino@aol.com>
2021-03-02 17:09:48 -0500

    Moved file path for burger pic

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.static('images'));
+// app.use(express.static(path.join(__dirname, 'views')));
 
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
